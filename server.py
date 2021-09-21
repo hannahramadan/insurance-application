a7739779cac4be40fdb490e198999376ee698daf
Hannah Ramadan <hannahr315@gmail.com>
2021-09-21 20:35:10 +0000

    General liability form routes and pdf added

--- server.py
+++ server.py
@@ -61,6 +61,38 @@ def builders_risk_form():
 
     return response
 
+@app.route('/general_liability_form', methods=['POST'])
+def general_liability_form():
+    email = request.form.get('email')
+    company_name = request.form.get('company_name')
+    zip_code = request.form.get('zip_code')
+    project_description = request.form.get('project_description')
+    business_type = request.form.get('business_type')
+
+    crud.create_builders_risk(email, company_name, zip_code, 
+                        project_description, business_type)
+
+    rendered = render_template('general_liability_pdf.html',email=email, company_name=company_name, zip_code=zip_code, 
+                        project_description=project_description, business_type=business_type)
+
+    display = Display(visible=0, size=(1024, 768))
+
+    try:
+        display.start()
+        pdf = pdfkit.from_string(rendered, False)
+    finally:
+        display.stop()
+
+    response = make_response(pdf)
+
+    # tells brower its about to get a pdf file
+    response.headers['Content-Type'] = 'application/pdf'
+    # loads pdf in browers vs download
+    response.headers['Content-Disposition'] = 'inline; filename=output.pdf'
+
+    return response
+
+
 if __name__ == '__main__':
     connect_to_db(app)
     app.run(host='0.0.0.0', debug=True)
